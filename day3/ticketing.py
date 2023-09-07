b58cb369ac3bcab56aa2d5382c3403bcbe665563
Henok Ashine <henokashine@Henoks-MBP.lan>
2023-09-07 17:37:37 -0400

    ticketing program

---
+++ day3/ticketing.py
@@ -0,0 +1,7 @@
+print("Welcome to Smyrna RollerCoster Park! \n")
+print("Lets us check your height before you aboard the RollerCoster \n")
+height = int(input("whats your feet in cm? "))
+if height > 120:
+    print("you are okay to ride !")
+else:
+    print("you nedd to grow a little bit longer! ")
