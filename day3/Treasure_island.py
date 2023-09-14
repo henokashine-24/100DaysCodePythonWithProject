f7899c714507fc13f858b1a4c2da6128cf00aeeb
Henok Ashine <henokashine@Henoks-MBP.lan>
2023-09-14 00:15:32 -0400

    Treasure island game

--- day3/Treasure_island.py
+++ day3/Treasure_island.py
@@ -1,20 +1,19 @@
 # in this exercise we are hunting treasure 
-
+print("\n")
 print("Welcome to the treasure island \n")
-print("Your mission is the find the Treasure!\n ")
-
-left_right = input("You are the cross road. where do you want to go? type 'left' or type 'right' \n")
-if left_right == "left":
-    wait_swim = input("You come to the lake. There is an island in the middle of the lake.. type 'wait' to wait for the boat or type 'swim' to swim across \n")
-    if wait_swim == "wait":
-        ryb= input("You are arrived at island unharmed.There is a house with 3 doors. one red , one yellow and one blue.which color do you choose? \n")
-        if ryb == "blue":
-            print("You enter the room of beasts. Game over.")
-        elif ryb =="red":
-            print("You are the at red house and you are safe..")
+print("Your mission is to find the Treasure!\n ")
+choice1 = input('You\'re at the cross road.Where do you want to go? Type "left" or "right" \n' ).lower()
+if choice1 == "right":
+    print("there is no road this way. Game over for you.")
+if choice1 == "left":
+    choice2 = input('You\'ve come to the lake.There is an island in the middle of the lake. Type "wait" to wait for a boat.Type "swim" to swim across. \n').lower()
+    if choice2 == "wait":
+        choice3 =input('You arrived at island unharmed.There is a house with three doors.One red, one yellow and one blue.which color do you choose? \n').lower()
+        if choice3 == "red":
+            print("Fire house. Game Over \n")
+        elif choice3 == "yellow":
+            print("You found the reasure. You won\n")
+        elif choice3 == "blue":
+            print("you enter the beast house.Game over for you!\n")
         else:
-            print("you are at the yellow house and you are safe as well . ")
-    else:
-        print("Game over for you as you are going to be devoured by the crocdiale in the lake!")
-else:
-    print("Game Over for you as you pick right. ")
+            print("Please type the choice given.")
