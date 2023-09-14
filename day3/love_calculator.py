fc712327cb8ad72f37548858798315cc74cd5a35
Henok Ashine <henokashine@Henoks-MBP.lan>
2023-09-13 20:47:51 -0400

    Love Calculator

--- day3/love_calculator.py
+++ day3/love_calculator.py
@@ -1,22 +1,26 @@
-print("Welcome to Love Calculator! \n ")
-name1 = input("what is your name? \n ")
-name2 = input("what is their name \n ")
-combined_string = name1 + name2
-lower_case = combined_string.lower()
+print("Welcome to love calculator ")
 
-t=name1.count("t")
-r=name1.count("r")
-u=name1.count("u")
-e=name1.count("e")
-true = t+r+u+e
-l=name1.count("l")
-o=name1.count("o")
-v=name1.count("v")
-e=name1.count("e")
-love = l+o+v+e
+name1 = input("what is your name? ")
+name2 = input("whats their name? ")
 
-love_score = str(love)+ str(true)
+combined_name = name1 + name2
+lower_name =combined_name.lower()
+t = lower_name.count("t")
+r = lower_name.count("r")
+u = lower_name.count("u")
+e = lower_name.count("e")
+first_digit = t+r+u+e
+l = lower_name.count("l")
+o = lower_name.count("o")
+v = lower_name.count("v")
+e = lower_name.count("e")
+second_digit = l+o+v+e
 
-print(f"there are :{love_score} characters in these names")
+score = int(str(first_digit) + str(second_digit))
 
-
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
---
+++ day4/test_love_cal.py
@@ -0,0 +1,6 @@
+# we are building a love calculator
+
+print("Welcome to love Calculator ! ")
+
+name1 = input("what is your name ? ")
+name2 = input("what is their name ? ")
